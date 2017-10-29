410984323bdc29b6dc3c224a5f3008c740b0d4bc
Hassan0z <hassan.rehman@cricingif.com>
2017-10-30 02:46:53 +0500

    Fixing port in server file

--- src/main.server.ts
+++ src/main.server.ts
@@ -10,7 +10,7 @@ import { ROUTES } from './routes';
 import { enableProdMode } from '@angular/core';
 enableProdMode();
 const app = express();
-const port = 8000;
+const port = (process.env.PORT || 8000);
 const baseUrl = `http://localhost:${port}`;
 
 app.engine('html', ngExpressEngine({
